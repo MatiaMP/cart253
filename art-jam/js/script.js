13bfd747f379610aa3b4f3b2ee1ac7e393738f8c
Matia Paki <matia.m.paki@gmail.com>
2025-09-21 13:30:36 -0400

    Started the art jam project

---
+++ art-jam/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Art Jam - Self-Portrait
+ * Matia Paki
+ * 
+ * This is a project using js and p5 to make a self-portrait
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
