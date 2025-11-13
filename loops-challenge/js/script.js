802deff36bd926572bd2202f7aafb10fcd3fbfd3
Matia Paki <matia.m.paki@gmail.com>
2025-11-12 22:24:11 -0500

    finished step 1

--- loops-challenge/js/script.js
+++ loops-challenge/js/script.js
@@ -21,36 +21,14 @@ function setup() {
 function draw() {
     background("pink");
     
-    stroke(0);
-    line(0, 0, 0, height);
-    
-    stroke(25);
-    line(50, 0, 50, height);
-    
-    stroke(50);
-    line(100, 0, 100, height);
-    
-    stroke(75);
-    line(150, 0, 150, height);
-    
-    stroke(100);
-    line(200, 0, 200, height);
-    
-    stroke(125);
-    line(250, 0, 250, height);
-    
-    stroke(150);
-    line(300, 0, 300, height);
-    
-    stroke(175);
-    line(350, 0, 350, height);
-    
-    stroke(200);
-    line(400, 0, 400, height);
-    
-    stroke(225);
-    line(450, 0, 450, height);
-    
-    stroke(250);
-    line(500, 0, 500, height);
+    let x = 0;
+    let color = 0;
+    let separation = 50;
+
+    while (x <= width){
+        stroke(color);
+        line(x, 0, x, height);
+        x += separation;
+        color += 25;
+    }
 }
